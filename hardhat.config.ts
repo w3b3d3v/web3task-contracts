e2b6f21055fd6e9a246797a2ab3ceed6e8523ba6
Axis, the high wizard <asguilift@hotmail.com>
2023-09-20 22:59:24 +0700

    Better environment configuration
    add: more packages, yarn.lock to ignore, version update to 8.2.0 and polygon mainnet added
    rm: bad importations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,33 +1,35 @@
 import { HardhatUserConfig } from "hardhat/config";
-import dotenv from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-chai-matchers";
-import "hardhat-gas-reporter";
-
+import dotenv from "dotenv";
 dotenv.config();
+
 const { POLYGON_URL, MUMBAI_URL, PRIVATE_KEY, ETHERSCAN_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-  networks: {
-    hardhat: {
-      chainId: 137,
-      forking: {
-        url: `${POLYGON_URL}`,
-        blockNumber: 38359528
-      }
-    },
-    mumbai: {
-      url: `${MUMBAI_URL}`,
-      accounts: [`${PRIVATE_KEY}`]
-    }
-  },
-  gasReporter: {
-    enabled: true,
-  },
-  etherscan: {
-    apiKey: `${ETHERSCAN_KEY}`
-  }
+	solidity: "0.8.20",
+	networks: {
+		hardhat: {
+			chainId: 31337,
+			forking: {
+				url: `${POLYGON_URL}`,
+				blockNumber: 38359528,
+			},
+		},
+		mumbai: {
+			url: `${MUMBAI_URL}`,
+			accounts: [`${PRIVATE_KEY}`],
+		},
+		polygon: {
+			url: `${POLYGON_URL}`,
+			accounts: [`${PRIVATE_KEY}`],
+		},
+	},
+	gasReporter: {
+		enabled: true,
+	},
+	etherscan: {
+		apiKey: `${ETHERSCAN_KEY}`,
+	},
 };
 
 export default config;
