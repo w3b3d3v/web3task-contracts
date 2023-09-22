109ae4f1ef158c248237d0bee6a4b76de502784c
Axis, the high wizard <asguilift@hotmail.com>
2023-09-22 17:45:48 +0700

    Audit, second part
    - Added NatSpec to all contracts
    - Changed Name of some function
    - Created owner variable
    - Removed fixed auth == 1
    - Owner can operate all functions without being operator
    - Added corresponding tests for features

--- contracts/AccessControl.sol
+++ contracts/AccessControl.sol
@@ -7,22 +7,42 @@ abstract contract AccessControl {
      */
     error Unauthorized(address operator);
 
-    error InvalidAuthId();
+    /**
+     * @dev Emitted when `authorizationId` (role) is invalid.
+     */
+    error InvalidAuthId(uint256 authId);
 
-    event AuthorizedPersonnel(
-        uint256 indexed id,
+    /**
+     * @dev Emitted when a new address is added to an `authorizationId` (role).
+     */
+    event AuthorizePersonnel(
+        uint256 indexed authorizationId,
         address indexed addr,
         bool authorized
     );
-    event AuthorizedOperator(uint256 indexed id, bytes4 indexed interfaceId);
 
+    /**
+     * @dev Emitted when an `authorizationId` (role) is added as an operator of
+     * a function in the contract.
+     */
+    event AuthorizeOperator(
+        uint256 indexed authorizationId,
+        bytes4 indexed interfaceId
+    );
+
+    /// @dev The owner of the contract.
+    address private _owner;
+
+    /// @dev Mapping of `authorizationId` and `address` to boolean.
     mapping(uint256 => mapping(address => bool)) private _auths;
+
+    /// @dev Mapping of `interfaceId` and `authorizationId` to boolean.
     mapping(bytes4 => mapping(uint256 => bool)) private _funcAuths;
 
-    constructor() {
-        _auths[1][msg.sender] = true;
-    }
-
+    /**
+     * @dev Modifier to check if `msg.sender` is authorized to operate a
+     * given interfaceId from one of the contract's function.
+     */
     modifier onlyOperator(
         bytes4 _interfaceId,
         uint256 _authorizationId,
@@ -36,50 +56,111 @@ abstract contract AccessControl {
         }
         _;
     }
-
+    /**
+     * @dev Modifier to check if `msg.sender` is the owner of the contract.
+     */
     modifier onlyOwner() {
-        if (!hasAuthorization(1, msg.sender)) {
+        if (_owner != msg.sender) {
             revert Unauthorized(msg.sender);
         }
         _;
     }
 
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev This function sets an authorization to an address.
+     *
+     * Emits a {AuthorizePersonnel} event.
+     *
+     * Requirements:
+     *
+     * - `msg.sender` must be the owner of the contract.
+     * - `_authorizationId` must not be 0.
+     */
     function setAuthorization(
         uint256 _authorizationId,
         address _authorizedAddress,
         bool _isAuthorized
     ) public virtual onlyOwner {
-        if (_authorizationId == 0 || _authorizationId == 1) {
-            revert InvalidAuthId();
+        if (_authorizationId == 0) {
+            revert InvalidAuthId(_authorizationId);
         }
+
         _auths[_authorizationId][_authorizedAddress] = _isAuthorized;
-        emit AuthorizedPersonnel(
+
+        emit AuthorizePersonnel(
             _authorizationId,
             _authorizedAddress,
             _isAuthorized
         );
     }
 
+    /**
+     * @dev This function sets an authorized role as the operator of a
+     * given interface id.
+     *
+     * Emits a {AuthorizeOperator} event.
+     *
+     * Requirements:
+     *
+     * - `msg.sender` must be the owner of the contract.
+     * - `_authorizationId` must not be 0.
+     */
     function setOperator(
         bytes4 _interfaceId,
         uint256 _authorizationId,
         bool _isAuthorized
     ) public virtual onlyOwner {
+        if (_authorizationId == 0) {
+            revert InvalidAuthId(_authorizationId);
+        }
+
         _funcAuths[_interfaceId][_authorizationId] = _isAuthorized;
-        emit AuthorizedOperator(_authorizationId, _interfaceId);
+
+        emit AuthorizeOperator(_authorizationId, _interfaceId);
     }
 
+    /**
+     * @dev This function checks if an address holds a given `authorizationId`.
+     *
+     * NOTE: The owner of the contract is always authorized.
+     */
     function hasAuthorization(
         uint256 _authorizationId,
         address _address
     ) public view virtual returns (bool) {
+        if (owner() == msg.sender) {
+            return true;
+        }
         return _auths[_authorizationId][_address];
     }
 
+    /**
+     * @dev This function checks if an `authorizedId` is allowed to operate
+     * a given `_interfaceId`.
+     *
+     * NOTE: The owner of the contract is always authorized.
+     */
     function isOperator(
         bytes4 _interfaceId,
         uint256 _authorizationId
     ) public view virtual returns (bool) {
+        if (owner() == msg.sender) {
+            return true;
+        }
         return _funcAuths[_interfaceId][_authorizationId];
     }
 }
--- contracts/TasksManager.sol
+++ contracts/TasksManager.sol
@@ -6,22 +6,45 @@ import {AccessControl} from "./AccessControl.sol";
 import {Multicall} from "@openzeppelin/contracts/utils/Multicall.sol";
 
 contract TasksManager is AccessControl, Web3Task, Multicall {
+    /// @dev Emitted when a task cannot be changed.
     error TaskCannotBeChanged(uint256 _taskId);
+
+    /// @dev Emitted when a title is updated.
     event TitleUpdated(uint256 indexed _taskId);
+
+    /// @dev Emitted when a description is updated.
     event DescriptionUpdated(uint256 indexed _taskId);
+
+    /// @dev Emitted when an end date is updated.
     event EndDateUpdated(uint256 indexed _taskId);
+
+    /// @dev Emitted when metadata is updated.
     event MetadataUpdated(uint256 indexed _taskId);
 
-    function isConcluded(uint256 _taskId) public view returns (bool) {
-        if (
-            _tasks[_taskId].status == Status.Completed ||
-            _tasks[_taskId].status == Status.Canceled
-        ) {
+    /**
+     * @dev This function checks if a task is concluded or canceled.
+     */
+    function isTerminated(uint256 _taskId) public view returns (bool) {
+        Task memory task = getTask(_taskId);
+
+        if (task.status == Status.Completed || task.status == Status.Canceled) {
             return true;
         }
+
         return false;
     }
 
+    /**
+     * @dev This function sets a new `title` for a task.
+     *
+     * Emits a {TitleUpdated} event.
+     *
+     * Requirements:
+     *
+     * - `_taskId` must be a valid task id.
+     * - `task.status` must not be `Status.Completed` or `Status.Canceled`.
+     * - `msg.sender` must be an authorized operator (see {AccessControl}).
+     */
     function setTitle(
         uint256 _taskId,
         uint256 _authorized,
@@ -31,13 +54,26 @@ contract TasksManager is AccessControl, Web3Task, Multicall {
         virtual
         onlyOperator(this.setTitle.selector, _authorized, msg.sender)
     {
-        if (isConcluded(_taskId)) {
+        if (isTerminated(_taskId)) {
             revert TaskCannotBeChanged(_taskId);
         }
+
         _tasks[_taskId].title = _title;
+
         emit TitleUpdated(_taskId);
     }
 
+    /**
+     * @dev This function sets a new `description` for a task.
+     *
+     * Emits a {DescriptionUpdated} event.
+     *
+     * Requirements:
+     *
+     * - `_taskId` must be a valid task id.
+     * - `task.status` must not be `Status.Completed` or `Status.Canceled`.
+     * - `msg.sender` must be an authorized operator (see {AccessControl}).
+     */
     function setDescription(
         uint256 _taskId,
         uint256 _authorized,
@@ -47,13 +83,26 @@ contract TasksManager is AccessControl, Web3Task, Multicall {
         virtual
         onlyOperator(this.setDescription.selector, _authorized, msg.sender)
     {
-        if (isConcluded(_taskId)) {
+        if (isTerminated(_taskId)) {
             revert TaskCannotBeChanged(_taskId);
         }
+
         _tasks[_taskId].description = _description;
+
         emit DescriptionUpdated(_taskId);
     }
 
+    /**
+     * @dev This function sets a new `endDate` for a task.
+     *
+     * Emits a {EndDateUpdated} event.
+     *
+     * Requirements:
+     *
+     * - `_taskId` must be a valid task id.
+     * - `task.status` must not be `Status.Completed` or `Status.Canceled`.
+     * - `msg.sender` must be an authorized operator (see {AccessControl}).
+     */
     function setEndDate(
         uint256 _taskId,
         uint256 _authorized,
@@ -63,13 +112,28 @@ contract TasksManager is AccessControl, Web3Task, Multicall {
         virtual
         onlyOperator(this.setEndDate.selector, _authorized, msg.sender)
     {
-        if (isConcluded(_taskId)) {
+        if (isTerminated(_taskId)) {
             revert TaskCannotBeChanged(_taskId);
         }
+
         _tasks[_taskId].endDate = _endDate;
+
         emit EndDateUpdated(_taskId);
     }
 
+    /**
+     * @dev This function sets a new `metadata` for a task.
+     *
+     * Emits a {MetadataUpdated} event.
+     *
+     * Requirements:
+     *
+     * - `_taskId` must be a valid task id.
+     * - `msg.sender` must be an authorized operator (see {AccessControl}).
+     *
+     * NOTE: This function is not restricted by the task status. It can be
+     * called at any time by the authorized operators.
+     */
     function setMetadata(
         uint256 _taskId,
         uint256 _authorized,
@@ -79,7 +143,12 @@ contract TasksManager is AccessControl, Web3Task, Multicall {
         virtual
         onlyOperator(this.setMetadata.selector, _authorized, msg.sender)
     {
+        if (_tasks[_taskId].endDate < block.timestamp) {
+            revert TaskCannotBeChanged(_taskId);
+        }
+
         _tasks[_taskId].metadata = _metadata;
+
         emit MetadataUpdated(_taskId);
     }
 }
