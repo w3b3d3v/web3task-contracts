ccfcf07ca76d082cdd235b92e028bbef515b5223
Rafael de Souza Sanches <rafaeldss12@gmail.com>
2023-10-07 03:43:49 -0300

    new unitary tests for the Web3Task contract

--- contracts/Web3Task.sol
+++ contracts/Web3Task.sol
@@ -69,6 +69,10 @@ abstract contract Web3Task is ERC721, AccessControl, IWeb3Task {
             revert InsufficientBalance(balance, _task.reward);
         }
 
+        if (_task.reward <= 0) {
+            revert InvalidReward(_task.reward);
+        }
+
         // Overflow not possible: taskId <= max uint256.
         unchecked {
             taskId++;
@@ -185,7 +189,7 @@ abstract contract Web3Task is ERC721, AccessControl, IWeb3Task {
         _alreadyVoted[_taskId] = msg.sender;
         _approvals[_taskId]++;
 
-        if (_approvals[_taskId] == APPROVALS) {
+        if (_approvals[_taskId] >= APPROVALS) {
             _tasks[_taskId].status = Status.Completed;
 
             _mint(task.assignee, _taskId);
@@ -314,7 +318,7 @@ abstract contract Web3Task is ERC721, AccessControl, IWeb3Task {
      *
      * Since the entire mapping for role structure cannot be looped,
      * we ask for the user to point the role he is using to call
-     * the function. We then check if the auth provided matches the any
+     * the function. We then check if the auth provided matches  any
      * of the initially auths, settled in the current task.
      */
     function _isRoleAllowed(
